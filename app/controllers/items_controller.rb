d271bcf281a48680c9305a75a16c85e9d07f7a0e
yuhei.nagano <yuheinagano8942@gmail.com>
2018-09-29 13:11:58 +0000

    Have

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,43 @@
+class ItemsController < ApplicationController
+  before_action :require_user_logged_in
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+    @have_haves = @item.have_users
+  end
+
+  def new
+    @items = []
+
+    @keyword = params[:keyword]
+    if @keyword.present? # 
+      results = RakutenWebService::Ichiba::Item.search({
+        keyword: @keyword,
+        imageFlag: 1,
+        hits: 20,
+      })
+
+      results.each do |result|
+        item = Item.find_or_initialize_by(read(result))
+        @items << item
+      end
+    end
+  end
+
+  private
+
+  def read(result)
+    code = result['itemCode']
+    name = result['itemName']
+    url = result['itemUrl']
+    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+
+    {
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
+  end
+end
---
+++ app/controllers/ownerships_controller.rb
@@ -0,0 +1,40 @@
+class OwnershipsController < ApplicationController
+  def create
+    @item = Item.find_or_initialize_by(code: params[:item_code])
+    
+    unless @item.persisted?
+    
+      results = RakutenWebService::Ichiba::Item.search(itemCode: @item.code)
+    
+      @item = Item.new(read(results.first))
+      @item.save
+    end
+  
+  if params[:type] == 'Want'
+    current_user.want(@item)
+    flash[:success] = '商品を Want しました'
+    
+  elsif params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
+  end
+  
+  
+   redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @item = Item.find(params[:item_id])
+    
+    if params[:type] == 'Want'
+      current_user.unwant(@item)
+      flash[:success] = '商品の Want を解除しました。'
+      
+  elsif params[:type] == 'Have'
+     current_user.unhave(@item)
+     flash[:success] = '商品の Have を解除しました。'
+    end
+    
+      redirect_back(fallback_location: root_path)
+  end
+end
