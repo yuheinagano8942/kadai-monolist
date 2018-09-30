36896114b4ec77d186c63e632212a16c4a9bb82f
yuhei.nagano <yuheinagano8942@gmail.com>
2018-09-30 10:39:46 +0000

    HaveCorrection

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @want_users = @item.want_users
-    @have_haves = @item.have_users
+    @have_users = @item.have_users
   end
 
   def new
